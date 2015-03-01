621c7b42ce56362722377753877d549c80e30522
Martin <Martin@Martin-PC.mshome.net>
2015-03-01 19:48:00 +0100

    Avancement de l'ajout d'un compte

--- FindAroundMe/src/com/example/findaroundme/MainActivity.java
+++ FindAroundMe/src/com/example/findaroundme/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.findaroundme;
 import ressources.Compte;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -11,26 +12,38 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	private Button bInscrire = (Button) findViewById(R.id.btInscrire);
-	private EditText tUsr = (EditText) findViewById(R.id.tUsrN);
-	private EditText tPsw = (EditText) findViewById(R.id.tPsw);
-	private EditText tMail = (EditText) findViewById(R.id.tMail);
-	private EditText tDn = (EditText) findViewById(R.id.tDn);
-	private EditText tTel = (EditText) findViewById(R.id.tTel);
-	private EditText tCp = (EditText) findViewById(R.id.tCp);
-	
+	private Button bInscrire; 
+	private EditText tUsr; 
+	private EditText tPsw;
+	private EditText tMail;
+	private EditText tDn;
+	private EditText tTel;
+	private EditText tCp;
+	private Dialog box;
+
 	private Compte compte = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		box = new Dialog(this);
+		bInscrire = (Button) findViewById(R.id.btInscrire);
+		tUsr = (EditText) findViewById(R.id.tUsrN);
+		tPsw = (EditText) findViewById(R.id.tPsw);
+		tMail = (EditText) findViewById(R.id.tMail);
+		tDn = (EditText) findViewById(R.id.tDn);
+		tTel = (EditText) findViewById(R.id.tTel);
+		tCp = (EditText) findViewById(R.id.tCp);
 		bInscrire.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				compte = new Compte(tUsr.toString(),tPsw.toString(),tMail.toString(),tDn.toString(),tTel.toString(),tCp.toString());
+				box.setContentView(R.layout.boite_dial);
+				box.setTitle("Yahoo !");
+				box.show();
+
 			}
 		});
 	}
@@ -40,6 +53,7 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		
 	}
 
 }
